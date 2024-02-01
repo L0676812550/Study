d68283f49bf5cad362a1189adff737e460074bb1
Liliia Mykhaliuk <L0676812550@gmail.com>
2024-02-01 18:17:57 +0200

    +

--- js/script.js
+++ js/script.js
@@ -1,6 +1,127 @@
-let user = {
-  name: Lily,
-  age: 30
-};
+//***1***/
+// Робота зі змінними 
+// Оголосіть дві змінні: name та city. 
+// Присвойте значення "Іван" змінній name. 
+// Скопіюйте значення зі змінної name в city. 
+// Виведіть значення змінної city, використовуючи функцію console.log (яка повинна показати “Іван”).
 
-console.log (user);
+// let user = {
+//   name: 'Іван',
+//   city: 'Іван',
+// };
+
+// console.log (user.city); //Іван
+
+//***2***/
+//Який буде результат виконання скрипта? 
+// let name = "Olga"; 
+// console.log(`привіт ${1}`); //привіт 1
+// console.log(`привіт ${"name"}`); // привіт name
+// console.log(`привіт ${name}`); //привіт Olga
+
+//***3***/
+//Видобути число зі змінних 
+// let a = "5"; 
+// let b = "13cvb"; 
+// let c = "12.9sxdcfgv"; 
+// // вивести в консоль тип
+// console.log(typeof Number(a));
+
+// console.log(Number.parseInt(b));
+// console.log(typeof Number.parseInt(b));
+
+// console.log(Number.parseFloat(c));
+// console.log(typeof Number.parseFloat(c));
+
+
+//***4*** 
+//Зробіть, щоб 0.1 + 0.2 = 0.3 
+// console.log((0.1*10 + 0.2*10) / 10);
+
+//***5** 
+//Поверніть найбільше число с набору 20, 10, 50, 40
+// console.log(Math.max(20, 10, 50, 40));
+
+//***6** 
+//Поверніть випадкове число в діапазоні від 2 до 4 
+// console.log(Math.random() * (4 - 2) + 2);
+
+//***7** 
+//дізнатись довжину message 
+// const message = "Welcome to Bahamas!"; 
+// console.log(message.length);
+
+//***8** 
+//вивести в консоль message великими літерами 
+// console.log(message.toLocaleUpperCase());
+
+//***9** 
+// створити пустий об*єкт 
+// додати туди ім*я, вік і місто 
+// вивести результат в консоль 
+// видалити місто 
+// додати буль з ключем: like flowers 
+// вивести результат в консоль 
+
+// let user = {}
+// user.name = 'Oleg';
+// user.age = 30;
+// user.city= "Kyiv";
+// console.log(user);
+// delete user.city;
+// user['like flowers'] = true;
+// console.log(user);
+
+//***10** 
+// За допомогою циклу “for…in” вивести в консоль ключі і значення об*єкта
+// for (key in user) {
+//   console.log(key);
+//   console.log(user[key]);
+// }
+
+// ___________________________________________________________________________
+
+// Урок №2
+
+//***1** 
+// Якщо змінна більше нуля - виведіть true, менше - false 
+//Перевірте це на варіантах  1, 0, -3.
+
+// console.log(-3 > 0);
+
+//!!!! ***2** 
+// Якщо змінна ="test" - виведіть true, 
+// Перевірте це на варіантах  'test', "qwerty", true 
+
+//let a = "test";
+
+// switch (a) {
+//   case "test":
+//     console.log("test");
+//     break;
+//   case 'test':
+//     console.log('test');
+//     break;
+//   case "qwerty":
+//     console.log("qwerty");
+//     break;
+//   case true:
+//     console.log(true);
+//     break;
+// }
+
+//***3** 
+// Якщо змінна більше 10 -  відніміть 5, 
+//менше - додайте 5, результат виведіть в консоль 
+//Перевірте це на варіантах  1, 10, 13. 
+
+//***4** 
+//Зробіть сервіс який отримує число від 1 до 12 
+// виведіть місяць який дорівнює числу 
+
+console.log(Math.random() * (12 - 1) + 1);
+//!! console.log(Math.round()); //How to use in this case?
+
+//***5** 
+//Зробіть сервіс який отримує тризначне число 
+//Поверніть користувачу сумму цих чисел
