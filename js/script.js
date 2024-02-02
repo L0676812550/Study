a3cd16972125589b78939903783243b4bb931c28
Liliia Mykhaliuk <L0676812550@gmail.com>
2024-02-02 14:13:26 +0200

    Home Work #2

--- js/script.js
+++ js/script.js
@@ -7,8 +7,9 @@
 
 // let user = {
 //   name: 'Іван',
-//   city: 'Іван',
+//   city: 'кіїв',
 // };
+// user.city = user.name;
 
 // console.log (user.city); //Іван
 
@@ -24,7 +25,7 @@
 // let a = "5"; 
 // let b = "13cvb"; 
 // let c = "12.9sxdcfgv"; 
-// // вивести в консоль тип
+//  вивести в консоль тип
 // console.log(typeof Number(a));
 
 // console.log(Number.parseInt(b));
@@ -53,7 +54,7 @@
 
 //***8** 
 //вивести в консоль message великими літерами 
-// console.log(message.toLocaleUpperCase());
+// console.log(message.toUpperCase());
 
 //***9** 
 // створити пустий об*єкт 
@@ -63,7 +64,7 @@
 // додати буль з ключем: like flowers 
 // вивести результат в консоль 
 
-// let user = {}
+// let user = {};
 // user.name = 'Oleg';
 // user.age = 30;
 // user.city= "Kyiv";
@@ -83,17 +84,17 @@
 
 // Урок №2
 
-//***1** 
+//***1**------------------
 // Якщо змінна більше нуля - виведіть true, менше - false 
 //Перевірте це на варіантах  1, 0, -3.
 
 // console.log(-3 > 0);
 
-//!!!! ***2** 
+// ***2**-----------------
 // Якщо змінна ="test" - виведіть true, 
 // Перевірте це на варіантах  'test', "qwerty", true 
 
-//let a = "test";
+//const a = "test";
 
 // switch (a) {
 //   case "test":
@@ -110,18 +111,52 @@
 //     break;
 // }
 
-//***3** 
+//***3**----------------
 // Якщо змінна більше 10 -  відніміть 5, 
 //менше - додайте 5, результат виведіть в консоль 
 //Перевірте це на варіантах  1, 10, 13. 
 
-//***4** 
+// let a = 1;
+// let b = 10;
+// let c = 13;
+
+// console.log(a > 10 ? a + 5 : a - 5);
+// console.log(b > 10 ? b + 5 : b - 5);
+// console.log(c > 10 ? c + 5 : c - 5);
+
+//***4**-----------------
 //Зробіть сервіс який отримує число від 1 до 12 
 // виведіть місяць який дорівнює числу 
 
-console.log(Math.random() * (12 - 1) + 1);
-//!! console.log(Math.round()); //How to use in this case?
+// const month = {
+//   1: "J",
+//   2: "F",
+//   3: "M",
+// };
 
-//***5** 
-//Зробіть сервіс який отримує тризначне число 
-//Поверніть користувачу сумму цих чисел
+// const m = prompt("Enter");
+
+// console.log(month[m]);
+
+//***5**-------------------- 
+// Зробіть сервіс який отримує тризначне число 
+// Поверніть користувачу сумму цих чисел
+
+// const num = prompt('Enter a number of 3 symbols');
+// if (num.length < 3) {
+//   alert("Please enter a correct 3-digit number.");
+// } else {
+//   const result = Number(num[0]) + Number(num[1]) + Number(num[2]);
+//   alert(`The sum of the digits in ${num} is: ${result}`);
+// };
+
+//!! Варіанти запису (Маша):
+// +'11' === Number('11');
+// String(11) === `${11}` === 11 + '';
+
+//№2 Рішення, з Машею:
+// const input = prompt('Enter number of 3 symbols');
+// const a = Number(input.charAt(0));
+// const b = +input.charAt(1);
+// const c = +input.charAt(2);
+// console.log(a + b + c);
